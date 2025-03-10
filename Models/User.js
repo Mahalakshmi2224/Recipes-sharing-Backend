b633223003f1cee604b11d4b08a78d03f2c3a19a
Mahalakshmi2224 <mahalakshmi71785@gmail.com>
2025-03-10 18:59:41 +0530

    adding all files in github

--- Models/User.js
+++ Models/User.js
@@ -1,9 +1,9 @@
-const mongoose=require('mongoose')
+import mongoose from "mongoose";
 
-const UserSchema=mongoose.Schema({
-    username:{type:String,require:true},
-    email:{type:String,require:true},
-    password:{type:String,require:true}
-})
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
+});
 
-module.exports=mongoose.model('User',UserSchema)
+export const UserModel = mongoose.model("users", UserSchema);
